5b3c2a1e385cf5adcbcf25b7d659b1c96727ba2b
nn31 <nn31@nn31s-MacBook-Pro.local>
2017-02-28 10:12:23 -0500

    wired up initializing {{credentials}} on about page

--- angular_app/app.module.js
+++ angular_app/app.module.js
@@ -1,17 +1,28 @@
 // maybe can use this to modularize code below
 // http://stackoverflow.com/questions/35203092/cannot-find-fs-in-controller
 
-app = angular.module('ddsDesktop', []);
+const fs = require('fs');
+const path = require('path');
+var app = angular.module('ddsDesktop', []);
+
+app.service('credentials', function() {
+    this.myFunc = function (x) {
+        if (fs.existsSync(x)) {
+            var notesByte = fs.readFileSync(x);
+            var notesJson = JSON.parse(notesByte);
+            return JSON.stringify(notesJson);
+        } else {
+            return 'Paste Software Agent Credentials here.';
+        }
+    }
+});
 
 app.controller('AboutController', [
-    '$scope',
-    function AboutController($scope) {
+    '$scope', 'credentials',
+    function AboutController($scope, credentials) {
         $scope.message = 'HOME PAGE';
+        $scope.creds = credentials.myFunc(path.join(process.env.HOME, '.ddsnodeclient'));
+        // $scope.creds = '{just a test}';
+
     }
 ]);
-
-
-// (() => {
-//
-// })();
-
